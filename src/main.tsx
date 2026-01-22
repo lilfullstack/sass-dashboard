a33628156cf94d94d449504c1d42bc3a31cc691e
Denise Horst <denisef.horst@gmail.com>
2026-01-22 14:31:09 -0300

    01 Shadcn UI setup and theme toggle

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+/**
+ * NODE MODULES
+ */
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+/**
+ * STYLES
+ */
+import '@/index.css';
+
+/**
+ * COMPONENTS
+ */
+import { App } from '@/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
